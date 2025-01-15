8a17204cf8aa34378d141786295d82f001f55a53
wilfred <wilf.funsho@gmail.com>
2025-01-15 07:31:54 +0100

    Layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return (
+        <main className="p-5">
+      <h1 className="text-4xl font-bold">About my GPT Chat</h1>
+    </main>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased px-2 md:px-5`}
       >
-        {children}
+        <header className="text-white font-bold bg-green-900 text-2xl">
+          <div className="flex flex-grow">
+            <Link href="/">GPT Chat</Link>
+            <Link href="/about" className="ml-5 font-light">
+              About
+            </Link>
+          </div>
+          <div></div>
+        </header>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex-geow">
+          {children}
+          </div>
+        </div>
       </body>
     </html>
   );
